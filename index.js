fc255989cf5ce508a90097911db7bc90f769f212
ArunDhoundiyal <arunprakash23797@gmail.com>
2024-01-30 09:29:25 +0000

    Super-Over-League React

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="card-container">
+    <h1 className="heading-style">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image-size"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image-size"
+      />
+    </div>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
